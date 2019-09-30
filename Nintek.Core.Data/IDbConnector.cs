30e7a057cbe0a3001ad7764668aeaa13a45e3520
Karol Hinz <karolhnz@gmail.com>
2019-09-30 21:45:33 +0200

    Add Nintek.Core.Data project with IUnitOfWork and IDbConnector

---
+++ Nintek.Core.Data/IDbConnector.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nintek.Core.Data
+{
+    public interface IDbConnector
+    {
+        Task<IDbConnection> Connect();
+    }
+}
---
+++ Nintek.Core.Data/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nintek.Core.Data
+{
+    public interface IUnitOfWork
+    {
+        event Action<IUnitOfWork> Commited;
+        IDbTransaction Transaction { get; }
+        Task Commit();
+    }
+}
