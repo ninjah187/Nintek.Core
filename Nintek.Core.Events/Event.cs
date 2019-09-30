98bb426a3089ce4a92d34cde40a0f3cda57485e5
Karol Hinz <karolhnz@gmail.com>
2019-09-30 20:03:59 +0200

    Add Events project

---
+++ Nintek.Core.Events/Event.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nintek.Core.Events
+{
+    public abstract class Event : IEvent
+    {
+        public int Id { get; }
+
+        protected Event()
+        {
+        }
+
+        protected Event(int id)
+        {
+            Id = id;
+        }
+    }
+}
---
+++ Nintek.Core.Events/IEventEmitter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nintek.Core.Events
+{
+    public interface IEventEmitter
+    {
+        EventQueue Events { get; }
+    }
+}
