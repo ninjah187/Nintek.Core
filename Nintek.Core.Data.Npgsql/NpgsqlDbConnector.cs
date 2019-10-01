42aa5a6c82ffe6fd0f29f32053ba267e070adff2
Karol Hinz <karolhnz@gmail.com>
2019-10-02 01:17:56 +0200

    Nintek.Core.Data.Npgsql project added

---
+++ Nintek.Core.Data.Npgsql/NpgsqlDbConnector.cs
@@ -0,0 +1,39 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nintek.Core.Data.Npgsql
+{
+    public class NpgsqlDbConnector : IDbConnector, IDisposable
+    {
+        public event Action<IDbConnection> Connected;
+
+        readonly string _connectionString;
+
+        NpgsqlConnection _connection;
+
+        public NpgsqlDbConnector(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public async Task<IDbConnection> Connect()
+        {
+            if (_connection == null)
+            {
+                _connection = new NpgsqlConnection(_connectionString);
+                await _connection.OpenAsync();
+                Connected?.Invoke(_connection);
+            }
+            return _connection;
+        }
+
+        public void Dispose()
+        {
+            _connection?.Dispose();
+        }
+    }
+}
---
+++ Nintek.Core.Data.Npgsql/NpgsqlUnitOfWork.cs
@@ -0,0 +1,49 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nintek.Core.Data.Npgsql
+{
+    public class NpgsqlUnitOfWork : IUnitOfWork
+    {
+        public event Action<IUnitOfWork> Commited;
+
+        public IDbConnector Connector { get; }
+
+        public IDbTransaction Transaction => _transaction;
+
+        NpgsqlTransaction _transaction;
+
+        public NpgsqlUnitOfWork(IDbConnector connector)
+        {
+            Connector = connector;
+            Connector.Connected += connection =>
+            {
+                StartTransactionIfNeeded((NpgsqlConnection) connection);
+            };
+        }
+
+        public async Task Commit()
+        {
+            if (_transaction == null)
+            {
+                return;
+            }
+            await _transaction.CommitAsync();
+            _transaction = null;
+            Commited?.Invoke(this);
+        }
+
+        void StartTransactionIfNeeded(NpgsqlConnection connection)
+        {
+            if (_transaction != null)
+            {
+                return;
+            }
+            _transaction = connection.BeginTransaction();
+        }
+    }
+}
