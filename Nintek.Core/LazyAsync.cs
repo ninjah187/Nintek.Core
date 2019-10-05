4baa2dcdb06f60b66749021ffe968eaf1a76538c
Karol Hinz <karolhnz@gmail.com>
2019-10-05 20:52:29 +0200

    LazyAsync<T> - class representing lazy async-loaded value

---
+++ Nintek.Core/LazyAsync.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nintek.Core
+{
+    public class LazyAsync<T>
+    {
+        readonly Provider<T> _provider;
+
+        T _value;
+        bool _isLoaded;
+
+        public LazyAsync(Provider<T> provider)
+        {
+            _provider = provider;
+        }
+
+        public async Task<T> GetValue()
+        {
+            if (!_isLoaded)
+            {
+                _value = await _provider();
+                _isLoaded = true;
+            }
+            return _value;
+        }
+
+        public TaskAwaiter<T> GetAwaiter()
+        {
+            return GetValue().GetAwaiter();
+        }
+    }
+
+    public static class LazyAsync
+    {
+        public static LazyAsync<T> From<T>(Provider<T> provider) => new LazyAsync<T>(provider);
+    }
+}
---
+++ Nintek.Core/Provider.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nintek.Core
+{
+    public delegate Task<T> Provider<T>();
+}
