0ecae90c7beb00d39a5a7b3267842ddb7980f22d
Karol Hinz <karolhnz@gmail.com>
2019-10-06 22:47:04 +0200

    Add EmailAddress value object

---
+++ Nintek.Core.Domain.Common/EmailAddress.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nintek.Core.Domain.Common
+{
+    public class EmailAddress : Text
+    {
+        public EmailAddress(Required<string> value) : base(value)
+        {
+            if (!EmailAddressValidator.IsValid(value))
+            {
+                throw new DomainException($"Invalid e-mail address format: {value}.");
+            }
+        }
+    }
+}
---
+++ Nintek.Core.Domain.Common/EmailAddressValidator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Nintek.Core.Domain.Common
+{
+    public static class EmailAddressValidator
+    {
+        static readonly EmailAddressAttribute _attribute = new EmailAddressAttribute();
+
+        public static bool IsValid(string email)
+        {
+            return _attribute.IsValid(email);
+        }
+    }
+}
