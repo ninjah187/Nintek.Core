5039f6dbb41d382c25a75ca7a309bf2dc9f00441
Karol Hinz <karolhnz@gmail.com>
2019-10-06 22:46:42 +0200

    Add Text value object

---
+++ Nintek.Core.Domain.Common/Text.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nintek.Core.Domain.Common
+{
+    public abstract class Text : ValueObject
+    {
+        public string Value { get; protected set; }
+
+        protected Text(
+            string value,
+            int minLength = 0,
+            int maxLength = int.MaxValue)
+        {
+            value = value ?? "";
+            if (value.Length < minLength)
+            {
+                throw new DomainException($"String's '{value}' length exceeded limit of minimum length: {minLength}.");
+            }
+            if (value.Length > maxLength)
+            {
+                throw new DomainException($"String's '{value}' length exceeded limit of maximum length: {maxLength}.");
+            }
+            Value = value;
+        }
+
+        protected Text(string value, int expectedLength)
+            : this(value, expectedLength, expectedLength)
+        {
+        }
+
+        protected override IEnumerable<object> GetEqualityMembers()
+        {
+            yield return Value;
+        }
+
+        public override string ToString()
+        {
+            return Value;
+        }
+
+        public static implicit operator string(Text obj)
+        {
+            return obj.Value;
+        }
+    }
+}
