afaff48dea4aaa054fa4505821303ad201d04328
Karol Hinz <karolhnz@gmail.com>
2020-02-03 18:48:15 +0100

    Add Nintek.Core.Gmail project

---
+++ Nintek.Core.Gmail/IMailService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nintek.Core.Gmail
+{
+    public interface IMailService
+    {
+        Task Send(string fromAddress, string toRecipients, string subject, string body = "", bool isBodyHtml = false);
+    }
+}
